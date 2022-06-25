472cb98938a47bc129c5c89a538735a633f494a6
artzzon <nexus.artem@gmail.com>
2022-06-26 00:29:27 +0300

    add change category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Categories() {
+  const [category, setCategory] = useState(0);
+
+  const onClickCategory = (index) => {
+    setCategory(index)
+  }
+
   return (
     <div className="categories">
     <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
+      <li onClick={() => onClickCategory(0)} className={category === 0 ? 'active' : ''}>Все</li>
+      <li onClick={() => onClickCategory(1)} className={category === 1 ? 'active' : ''}>Мясные</li>
+      <li onClick={() => onClickCategory(2)} className={category === 2 ? 'active' : ''}>Вегетарианская</li>
+      <li onClick={() => onClickCategory(3)} className={category === 3 ? 'active' : ''}>Гриль</li>
+      <li onClick={() => onClickCategory(4)} className={category === 4 ? 'active' : ''}>Острые</li>
+      <li onClick={() => onClickCategory(5)} className={category === 5 ? 'active' : ''}>Закрытые</li>
     </ul>
   </div>
   )
