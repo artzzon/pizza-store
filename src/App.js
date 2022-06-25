18e4ebbe89370c96ac9dd059bbda70c330655ffb
artzzon <nexus.artem@gmail.com>
2022-06-26 00:26:15 +0300

    add assets + add pizzas state

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import Pizza from './components/Pizza';
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <Pizza />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <Pizza title="Test" price="1" />
           </div>
         </div>
       </div>
