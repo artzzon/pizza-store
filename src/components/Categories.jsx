b168482829ea2c2f4c52b3f4681de1c4dd9ab6b4
artzzon <nexus.artem@gmail.com>
2022-06-26 17:03:29 +0300

    rendering categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,20 +3,18 @@ import React, { useState } from 'react'
 export default function Categories() {
   const [category, setCategory] = useState(0);
 
+  const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые']
+
   const onClickCategory = (index) => {
-    setCategory(index)
+    setCategory(index);
   }
 
   return (
     <div className="categories">
-    <ul>
-      <li onClick={() => onClickCategory(0)} className={category === 0 ? 'active' : ''}>Все</li>
-      <li onClick={() => onClickCategory(1)} className={category === 1 ? 'active' : ''}>Мясные</li>
-      <li onClick={() => onClickCategory(2)} className={category === 2 ? 'active' : ''}>Вегетарианская</li>
-      <li onClick={() => onClickCategory(3)} className={category === 3 ? 'active' : ''}>Гриль</li>
-      <li onClick={() => onClickCategory(4)} className={category === 4 ? 'active' : ''}>Острые</li>
-      <li onClick={() => onClickCategory(5)} className={category === 5 ? 'active' : ''}>Закрытые</li>
-    </ul>
-  </div>
+      <ul>
+        {categories.map((categoryItem, index) =>
+          <li onClick={() => onClickCategory(index)} className={category === index ? 'active' : ''}>{categoryItem}</li>)}
+      </ul>
+    </div>
   )
 }
