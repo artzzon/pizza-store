214368cccd38e5b2e44b61584446959312801b74
artzzon <nexus.artem@gmail.com>
2022-06-27 22:38:24 +0300

    fix adaptive

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Cart() {
   return (
@@ -386,7 +387,7 @@ export default function Cart() {
             <span> Сумма заказа: <b>900 ₽</b> </span>
           </div>
           <div className="cart__bottom-buttons">
-            <a href="/" className="button button--outline button--add go-back-btn">
+            <Link to="/" className="button button--outline button--add go-back-btn">
               <svg
                 width="8"
                 height="14"
@@ -404,7 +405,7 @@ export default function Cart() {
               </svg>
 
               <span>Вернуться назад</span>
-            </a>
+            </Link>
             <div className="button pay-btn">
               <span>Оплатить сейчас</span>
             </div>
