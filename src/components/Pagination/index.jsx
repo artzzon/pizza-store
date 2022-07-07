f17bd6bb0159f2db8c0e56cf91eef67b98044ba5
artzzon <nexus.artem@gmail.com>
2022-07-07 16:00:57 +0300

    add pagination

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ReactPaginate from 'react-paginate';
 
-const Pagination = () => {
+import styles from './Pagination.module.scss'
+
+const Pagination = ({ onChangePage }) => {
   return (
-    <div></div>
+    <ReactPaginate
+        className={styles.root}
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={(event) => onChangePage(event.selected + 1)}
+        pageRangeDisplayed={4}
+        pageCount={3}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+      />
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import Pizza from '../components/Pizza';
 import Skeleton from '../components/Pizza/Skeleton';
+import Pagination from '../components/Pagination';
 
 export default function Home({ searchValue }) {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [category, setCategory] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [activeSortCategory, setActiveSortCategory] = useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -22,7 +24,7 @@ export default function Home({ searchValue }) {
     const currentCategory = category > 0 ? `category=${category}` : '';
     const search = searchValue ? `search=${searchValue}` : '';
 
-    fetch(`https://62b8a44403c36cb9b7ca1e33.mockapi.io/items?${currentCategory}&sortBy=${sortBy}&order=${order}${search}`
+    fetch(`https://62b8a44403c36cb9b7ca1e33.mockapi.io/items?page=${currentPage}&limit=4&${currentCategory}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -30,12 +32,20 @@ export default function Home({ searchValue }) {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [category, activeSortCategory, searchValue]);
+  }, [category, activeSortCategory, searchValue, currentPage]);
 
   const renderPizzas = pizzas
     //.filter(obj => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
     .map((pizza) => <Pizza key={pizza.id} {...pizza} />);
   const skeleton = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  // const handlePageClick = (event) => {
+  //   const newOffset = (event.selected * itemsPerPage) % items.length;
+  //   console.log(
+  //     `User requested page number ${event.selected}, which is offset ${newOffset}`
+  //   );
+  //   setItemOffset(newOffset);
+  // };
 
   return (
     <div className="container">
@@ -45,10 +55,9 @@ export default function Home({ searchValue }) {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {isLoading
-          ? skeleton
-          : renderPizzas}
+        {isLoading ? skeleton : renderPizzas}
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)}/>
     </div>
   )
 }
